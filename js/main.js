14c93bf6d9d6352004c5f97c288b0f72c1854ae0
RT Flores <rotyflo@gmail.com>
2018-07-11 19:13:46 -0500

    Add IDs and keep footer at bottom on entire site

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
 $(document).ready(function() {
-    $(".about-link").click(function() {
-        $(".about").show();
-        $(".portfolio").hide();
-        $(".contact").hide();
+    $("#about-link").click(function() {
+        $("#about").show();
+        $("#projects").hide();
+        $("#contact").hide();
 
-        $(".about-link").addClass("active");
-        $(".portfolio-link").removeClass("active");
-        $(".contact-link").removeClass("active");
+        $("#about-link").addClass("active");
+        $("#projects-link").removeClass("active");
+        $("#contact-link").removeClass("active");
     });
     
-    $(".portfolio-link").click(function() {
-        $(".about").hide();
-        $(".portfolio").show();
-        $(".contact").hide();
+    $("#projects-link").click(function() {
+        $("#about").hide();
+        $("#projects").show();
+        $("#contact").hide();
 
-        $(".about-link").removeClass("active");
-        $(".portfolio-link").addClass("active");
-        $(".contact-link").removeClass("active");
+        $("#about-link").removeClass("active");
+        $("#projects-link").addClass("active");
+        $("#contact-link").removeClass("active");
     });
     
-    $(".contact-link").click(function() {
-        $(".about").hide();
-        $(".portfolio").hide();
-        $(".contact").show();
+    $("#contact-link").click(function() {
+        $("#about").hide();
+        $("#projects").hide();
+        $("#contact").show();
 
-        $(".about-link").removeClass("active");
-        $(".portfolio-link").removeClass("active");
-        $(".contact-link").addClass("active");
+        $("#about-link").removeClass("active");
+        $("#projects-link").removeClass("active");
+        $("#contact-link").addClass("active");
     });
 });
