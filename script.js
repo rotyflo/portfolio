a1955031ba39fe8c4e34c296619ec8e2cdc8804b
RT <me@rtflores.com>
2021-09-26 18:09:51 -0700

    fix safari ios focus trapping (hopefully)

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ let terminalInput = document.getElementById("terminal-input");
 window.addEventListener("click", () => {
 	terminalInput.focus();
 });
+
+window.addEventListener("touchend", () => {
+	terminalInput.focus();
+});
