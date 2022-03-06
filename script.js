b63453f9fc5795f7ee5d29cae6d33ffb5df7554c
RT Flores <me@rtflores.com>
2022-03-05 21:41:25 -0800

    remove terminal prompt js

--- script.js
+++
@@ -1,9 +0,0 @@
-let terminalInput = document.getElementById("terminal-input");
-
-window.addEventListener("click", () => {
-	terminalInput.focus();
-});
-
-window.addEventListener("touchend", () => {
-	terminalInput.focus();
-});
