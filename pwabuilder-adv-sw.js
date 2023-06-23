1d0b13d2b5d0b0e21940f83a6dfa658159d47eca
RT <me@rtflores.com>
2023-06-23 11:19:15 -0700

    add offline mode via service workers

---
+++ pwabuilder-adv-sw.js
@@ -0,0 +1,3 @@
+
+  import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
+  precacheAndRoute([{"revision":"bb848ce44e95875db931013d48da4521","url":"css/index.css"},{"revision":"0f718cf4933ebf47b6ff5ec937aff968","url":"css/theme.css"},{"revision":"d0fcf06234ff1f7f54da99e6f7e1e01f","url":"how-to/get-local-ip-address.html"},{"revision":"bac683403a877ff9f2c9155c158d26cc","url":"how-to/ssh-without-a-password.html"},{"revision":"71d2956aef51359b848e078ea3083b27","url":"index.html"},{"revision":"ff5da0b292612186186be55009060875","url":"service-worker.js"}]);
